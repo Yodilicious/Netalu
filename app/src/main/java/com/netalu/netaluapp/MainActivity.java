44ce183c84e4df1a98997458c66d0262d9de33c0
Jodi Visser <visser.jodi@gmail.com>
2017-09-27 21:07:52 -0400

    Working on Login and Register Activities

--- app/src/main/java/com/netalu/netaluapp/MainActivity.java
+++ app/src/main/java/com/netalu/netaluapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.netalu.netaluapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchLoginActivity();
+            }
+        });
+    }
+
+    private void launchLoginActivity() {
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
