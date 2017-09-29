679a949338986cebcd9cd40a97692ec6bc996a04
Jodi Visser <visser.jodi@gmail.com>
2017-09-29 18:18:53 -0400

    Added Back Button and And Provinces for Drop Down

--- app/src/main/java/com/netalu/netaluapp/RegistrationActivity.java
+++ app/src/main/java/com/netalu/netaluapp/RegistrationActivity.java
@@ -1,21 +1,41 @@
 package com.netalu.netaluapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class RegistrationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private Button registerButton;
     private Spinner spinner;
-    private static final String[]paths = {"Alberta", "British Columbia", "Manitoba"};
+    private static final String[]paths =
+            {
+                    "Alberta",
+                    "British Columbia",
+                    "Manitoba",
+                    "New Brunswick",
+                    "Newfoundland and Labrador",
+                    "Nova Scotia",
+                    "Ontario",
+                    "Prince Edward Island",
+                    "Quebec",
+                    "Saskatchewan",
+                    "Northwest Territories",
+                    "Nunavut",
+                    "Yukon"
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        registerButton = (Button) findViewById(R.id.registrationSubmitButton);
 
         spinner = (Spinner)findViewById(R.id.provinceSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(RegistrationActivity.this,
@@ -24,6 +44,19 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchRegisterActivity();
+            }
+        });
+    }
+
+    private void launchRegisterActivity() {
+
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -38,10 +71,8 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
             case 2:
                 // Whatever you want to happen when the thrid item gets selected
                 break;
-
         }
     }
-
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
