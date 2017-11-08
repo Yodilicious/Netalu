94c63ae2633f6c7f993a6ccefe71ce86c5f282b5
Jodi Visser <visser.jodi@gmail.com>
2017-11-07 19:43:42 -0500

    Continue working on the Database

--- app/src/main/java/com/netalu/netaluapp/MainMenuActivity.java
+++ app/src/main/java/com/netalu/netaluapp/MainMenuActivity.java
@@ -1,5 +1,6 @@
 package com.netalu.netaluapp;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,11 +15,18 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    private void displayMainMenu() {
+
+        SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mDbOpenHelper = new NetaluOpenHelper(this);
+
+        displayMainMenu();
 
         setContentView(R.layout.activity_main_menu);
     }
